61dd95ec938526b99d14c0b9ec1bca1e1fc09b29
Unknown <bstanton773@gmail.com>
2018-03-02 13:02:57 -0600

    Added club page

--- hello.py
+++ hello.py
@@ -23,4 +23,26 @@ def index():
 
     return render_template('index.html', user = user, posts = posts, title = "Hello friend")
 
+@app.route('/club')
+def club():
+    plans = [
+        {
+            'title': 'Platinum',
+            'price': 1000
+        },
+        {
+            'title': 'Gold',
+            'price': 750
+        },
+        {
+            'title': 'Silver',
+            'price': 500
+        },
+        {
+            'title': 'Bronze',
+            'price': 250
+        }
+    ]
+    return render_template('club.html', plans = plans)
+
 app.run()
