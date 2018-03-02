a95aad61f76b22bddb6252176808f38480cd43b7
Unknown <bstanton773@gmail.com>
2018-03-02 13:56:59 -0600

    Adding flash message after login

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 from forms import LoginForm
 
 app = Flask(__name__)
@@ -50,6 +50,9 @@ def club():
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title = 'Sign In', form = form)
 
 app.run()
