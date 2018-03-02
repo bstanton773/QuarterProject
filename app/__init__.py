f7575f06d99f086d7cc960dbd4b1a130ab38ca9e
Unknown <bstanton773@gmail.com>
2018-03-02 14:28:45 -0600

    Create python side database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,24 @@
 from flask import Flask
 from config import Config
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config.from_object(Config)
 
+#Setup Database
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
 from app import routes
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
