62342752f3783634746290429dff725ffd425f98
Unknown <bstanton773@gmail.com>
2018-03-02 12:38:07 -0600

    Display list of posts

--- hello.py
+++ hello.py
@@ -6,6 +6,17 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     user = {'username': 'Brian'}
-    return render_template('index.html', user = user)
+    posts = [
+        {
+            'title': {'main': 'I hate Python', 'sub': 'this is a sad story'},
+            'body': 'Python can do every thing. However, it is very hard'
+        },
+        {
+            'title': {'main': 'I love Python', 'sub': 'this is a happy story'},
+            'body': 'Python can do every thing. However, it is very easy'
+        }
+    ]
+
+    return render_template('index.html', user = user, posts = posts)
 
 app.run()
