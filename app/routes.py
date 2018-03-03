9b1134b254d5c03b6557ce4a4e2138a1093bf1bc
Unknown <bstanton773@gmail.com>
2018-03-02 20:32:03 -0600

    Add login/logout

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 from app import app
 from app.forms import LoginForm
-from flask import render_template, redirect, flash
+from flask import render_template, redirect, flash, url_for
+from flask_login import current_user, login_user, logout_user
 
-from app.models import Post
+from app.models import Post, User
 
 @app.route('/')
 @app.route('/index')
@@ -35,8 +36,23 @@ def club():
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title = 'Sign In', form = form)
+    if form.validate_on_submit():  # post and submit validate
+        # get the user from data base use code
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+
+    return render_template('login.html', title = 'Sign In', form = form) # GET or submit validate Failed
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
